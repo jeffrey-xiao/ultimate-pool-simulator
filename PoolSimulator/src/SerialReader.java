5f897afde885d89dd2c4afb6b7938344aa1398eb
Daniel Tong <danielytong@hotmail.com>
2016-11-28 06:07:08 +0000

    Formatted
    git-svn-id: https://ecesvn.uwaterloo.ca/courses/se101/2016/teams/team61@2873 06e43772-ae1b-4c0d-9892-c0dabbf93334

--- PoolSimulator/src/SerialReader.java
+++ PoolSimulator/src/SerialReader.java
@@ -1,58 +1,48 @@
-import java.io.*;
-import java.util.*;
 import jssc.*;
 
-public class SerialReader {
+public class SerialReader implements SerialPortEventListener {
 	static SerialPort serialPort;
-	public static void main (String[] args) {
+	static StringBuilder msg = new StringBuilder();
+	boolean receivingMessage = false;
+	
+	public SerialReader (String comName) {
 		serialPort = new SerialPort("COM3");
+		initializePort();
+	}
+	
+	private void initializePort () {
 		try {
 			serialPort.openPort();
-
-			serialPort.setParams(SerialPort.BAUDRATE_9600,
-					SerialPort.DATABITS_8,
-					SerialPort.STOPBITS_1,
-					SerialPort.PARITY_NONE);
-
-			serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN | 
-					SerialPort.FLOWCONTROL_RTSCTS_OUT);
-
-			serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
-
-		}
-		catch (SerialPortException ex) {
-			System.out.println("There are an error on writing string to port 3: " + ex);
+			serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+			serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN | SerialPort.FLOWCONTROL_RTSCTS_OUT);
+			serialPort.addEventListener(this, SerialPort.MASK_RXCHAR);
+		} catch (SerialPortException ex) {
+			System.out.println("There is an error with writing/reading to port " + serialPort.getPortName() + ": " + ex);
 		}
 	}
-
-	private static class PortReader implements SerialPortEventListener {
-		StringBuilder msg = new StringBuilder();
-		boolean receivingMessage = false;
-		@Override
-		public void serialEvent(SerialPortEvent event) {
-			if(event.isRXCHAR() && event.getEventValue() > 0) {
-				try {
-					char[] buffer = serialPort.readString(event.getEventValue()).toCharArray();
-					for (char c : buffer) {
-						if (c == '>') {
-							receivingMessage = true;
-							msg.setLength(0);
-						} else if (receivingMessage) {
-							if (c == '\r') {
-								receivingMessage = false;
-								System.out.println("RECEIVED MESSAGE " + msg.toString());
-							} else {
-								msg.append((char)c);
-							}
+	
+	@Override
+	public void serialEvent (SerialPortEvent event) {
+		if(event.isRXCHAR() && event.getEventValue() > 0) {
+			try {
+				char[] buffer = serialPort.readString(event.getEventValue()).toCharArray();
+				for (char c : buffer) {
+					if (c == '>') {
+						receivingMessage = true;
+						msg.setLength(0);
+					} else if (receivingMessage) {
+						if (c == '\r') {
+							receivingMessage = false;
+							System.out.println("RECEIVED MESSAGE " + msg.toString());
+						} else {
+							msg.append((char)c);
 						}
 					}
 				}
-				catch (SerialPortException ex) {
-					System.out.println("Error in receiving string from COM-port: " + ex);
-					System.out.println("hello i have a small penis");
-				}
+			}
+			catch (SerialPortException ex) {
+				System.out.println("Error in receiving string from COM-port: " + ex);
 			}
 		}
-
 	}
 }
