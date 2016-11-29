395a1558f050a6667c35c06b8351f3e8fa5a001f
Daniel Tong <danielytong@hotmail.com>
2016-11-29 14:51:39 +0000

    Added user .ino files with switch, button and potentiometer input
    git-svn-id: https://ecesvn.uwaterloo.ca/courses/se101/2016/teams/team61@3011 06e43772-ae1b-4c0d-9892-c0dabbf93334

---
+++ PoolSimulator/bin/wire_util.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+
+/**
+ * You need to use your own Instance of the TwoWire Object (instead of the standard `Wire`)
+ * because they communicate over the wrong pins!
+ */
+static TwoWire orbitWire(0);
+
+void WireInit()
+{
+  orbitWire.begin();
+}
+
+void WireWriteByte(int address, uint8_t value)
+{
+  orbitWire.beginTransmission(address);
+  orbitWire.write(value);
+  orbitWire.endTransmission();
+}
+
+void WireWriteRegister(int address, uint8_t reg, uint8_t value)
+{
+  orbitWire.beginTransmission(address);
+  orbitWire.write(reg);
+  orbitWire.write(value);
+  orbitWire.endTransmission();
+}
+
+/**
+ * WireRequestArray(int address, uint32_t* buffer, uint8_t amount)
+ * 
+ * address - I2C Address of Chip
+ * buffer - Buffer to store data
+ * amount - Bytes of information to store!
+ */
+void WireRequestArray(int address, uint32_t* buffer, uint8_t amount)
+{
+  orbitWire.requestFrom(address, amount);
+  do 
+  {
+    while(!orbitWire.available());
+    *(buffer++) = orbitWire.read();
+  } while(--amount > 0);
+}
